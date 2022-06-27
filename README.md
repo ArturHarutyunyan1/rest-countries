6bf1bfb36dbefd30a9854fab7460fab313818da8
Artur Harutyunyan <Arturhar@outlook.com>
2022-06-27 16:31:30 +0400

    init

--- README.md
+++ README.md
@@ -21,7 +21,6 @@ Users should be able to:
 - See all countries from the API on the homepage
 - Search for a country using an `input` field
 - Filter countries by region
-- Click on a country to see more detailed information on a separate page
 - Toggle the color scheme between light and dark mode *(optional)*
 
 ### Screenshot
--- script.js
+++ script.js
@@ -1,102 +1,107 @@
+
 const switcher        = document.querySelector('#switcher')
 const theme           = window.localStorage.getItem('data-theme')
-const search          = document.querySelector('.input-value')
-const dropBtn         = document.querySelector('.drop-btn')
+const search          = document.querySelector('.searchValue')
+const dropBtn         = document.querySelector('.dropBtn')
 const dropdownItems   = document.querySelectorAll('.item')
-const detailsWindow   = document.querySelector('.details')
+const detailWindow   = document.querySelector('.details')
 const loader          = document.querySelector('.lds-roller')
 const dropdownContent = document.querySelector('.dropdown-items')
 
 async function getCountries(){
-    const res  = await fetch(`https://restcountries.com/v2/all`)
-    const data = await res.json()
-    displayCountries(data)
-    hideLoader()
+    try {
+        const res  = await fetch(`https://restcountries.com/v2/all`)
+        const data = await res.json()
+        displayCountries(data)
+        hideLoader()
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function displayCountries(data){
-    data.forEach(country => {
+    data.forEach(result => {
         const grid     = document.querySelector('.grid')
-        const item     = document.createElement('div')
-        item.classList.add('grid-item')
-        grid.append(item)
-        item.innerHTML = 
+        const gridItem = document.createElement('div')
+        gridItem.classList.add('grid-item')
+        grid.append(gridItem)
+        gridItem.innerHTML = 
         `
         <div class="flag">
-            <img src="${country.flags.png}">
+            <img src="${result.flags.png}">
         </div>
         <div class="desc">
-            <h3 class="name">${country.name}</h3>
-            <p><span>Population:</span> ${country.population}</p>
-            <p class="region"><span>Region:</span> ${country.region}</p>
-            <p><span>Capital:</span> ${country.capital}</p>
+
+        <h3 class="name">${result.name}</h3>
+            <p><span>Population: </span>${result.population}</p>
+            <p class="region"><span>Region: </span>${result.region}</p>
+            <p><span>Capital: </span>${result.capital}</p>
         </div>
         `
-
-        item.addEventListener('click', ()=>{
-           detailsWindow.classList.add('active')
-           countryDetails(country)
-           if(detailsWindow.classList.contains('active')){
-            document.body.style.overflow = 'hidden'
-           }
+        gridItem.addEventListener('click', ()=>{
+            detailWindow.classList.add('active')
+            showDetails(result)
+            if(detailWindow.classList.contains('active')){
+             document.body.style.overflow = 'hidden'
+            }
         })
     })
 }
 
-function countryDetails(country){
-    showLoader()
-    detailsWindow.innerHTML = 
+function showDetails(result){
+    detailWindow.innerHTML = 
     `
+    <div class="content">
     <button class="close"><i class="fa-solid fa-arrow-left"></i> <span class="text">Back</span></button>
-    <div class="row">
-        <div class="row-item">
-            <img src="${country.flags.svg}" id="flag">
-        </div>
-        <div class="row-item">
-            <div class="col">
-                <h1>${country.name}</h1>
-                <div class="row">
-                    <div class="row-item">
-                        <p><span>Native Name:</span> ${country.nativeName}</p>
-                        <p><span>Population:</span>  ${country.population}</p>
-                        <p><span>Region:</span>      ${country.region}</p>
-                        <p><span>Sub Region:</span>  ${country.subregion}</p>
-                        <p><span>Capital:</span>     ${country.capital}</p>
-                    </div>
-                    <div class="row-item">
-                        <p><span>Top Level Domain:</span> ${country.topLevelDomain}</p>
-                        <p><span>Currencies:</span>       ${country.currencies.map(currency => currency.code)}</p>
-                        <p><span>Languages:</span>        ${country.languages.map(languages => languages.name)}</p>
-                        <p><span>Border Countries:</span> ${country.borders}</p>
+        <div class="row">
+            <div class="row-item">
+                <img src="${result.flags.png}" id="flag">
+            </div>
+            <div class="row-item">
+                <div class="col">
+                <h1>${result.name}</h1>
+                    <div class="row">
+                        <div class="row-item">
+                            <p><span>Native Name: </span>${result.nativeName}</p>
+                            <p><span>Population: </span>${result.population}</p>
+                            <p><span>Region: </span>${result.region}</p>
+                            <p><span>Sub Region: </span>${result.subRegion}</p>
+                            <p><span>Capital: </span>${result.capital}</p>
+                        </div>
+                        <div class="row-item">
+                            <p><span>Top Level Domain: </span>${result.topLevelDomain}</p>
+                            <p><span>Currencies:</span>       ${result.currencies.map(currency => currency.code)}</p>
+                            <p><span>Languages:</span>        ${result.languages.map(languages => languages.name)}</p>
+                            <p><span>Border Countries:</span> ${result.borders}</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        <h3 class="author">Challenge by <a href="https://frontendmentor.io" target="_blank">Frontend Mentor</a> Coded by <a href="https://github.com/arturharutyunyan1" target="_blank"> Artur Harutyunyan</a></h3>
     </div>
-    <h3 class="author">Challenge by <a href="https://frontendmentor.io" target="_blank">Frontend Mentor</a> Coded by <a href="https://github.com/arturharutyunyan1" target="_blank"> Artur Harutyunyan</a></h3>
     `
-    hideLoader()
-    const closeButton   = document.querySelector('.close')
+    const closeBtn = document.querySelector('.close')
 
-    closeButton.addEventListener('click', ()=>{
-        detailsWindow.classList.remove('active')
-        document.body.style.overflow = 'visible'
+    closeBtn.addEventListener('click', ()=>{
+        if(detailWindow.classList.contains('active')){
+            detailWindow.classList.remove('active')
+            document.body.style.overflow = 'visible'
+        }
     })
-
 }
 
 search.addEventListener('input', (e)=>{
-    showLoader()
-    const {value}       = e.target
-    const countryName = document.querySelectorAll('.name')
-    countryName.forEach(name => {
+    const {value} = e.target
+    const countryName    = document.querySelectorAll('.name')
+
+    countryName.forEach(name=>{
         if(name.textContent.toLowerCase().includes(value.toLowerCase())){
             name.parentElement.parentElement.style.display = 'block'
         }else{
             name.parentElement.parentElement.style.display = 'none'
         }
     })
-    hideLoader()
 })
 
 dropBtn.addEventListener('click', ()=>{
@@ -109,14 +114,12 @@ dropdownItems.forEach(item => {
         const region = document.querySelectorAll('.region')
 
         region.forEach(reg => {
-            showLoader()
             if(reg.textContent.includes(value) || value === 'All'){
                 reg.parentElement.parentElement.style.display = 'block'
             }else{
                 reg.parentElement.parentElement.style.display = 'none'
             }
             dropdownContent.classList.remove('active-dropdown')
-            hideLoader()
         })
     })
 })
@@ -127,21 +130,15 @@ if(theme){
 
 switcher.checked = theme == 'dark' ? true : false
 
-switcher.addEventListener('change', function () {
+switcher.addEventListener('change', function(){
     if(this.checked){
-      document.documentElement.setAttribute('data-theme', 'dark')
-      window.localStorage.setItem('data-theme', 'dark')
-    } else if(!this.checked){
-      document.documentElement.setAttribute('data-theme', 'light')
-      window.localStorage.setItem('data-theme', 'light')
+        window.localStorage.setItem('data-theme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'dark')
+    }else if(!this.checked){
+        window.localStorage.setItem('data-theme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light')
     }
-});
-
-function showLoader(){
-    if(loader.classList.contains('loaded')){
-        loader.classList.remove('loaded')
-    }
-}
+})
 
 function hideLoader(){
     loader.classList.add('loaded')
